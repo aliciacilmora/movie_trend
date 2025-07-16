07e3e365449584b5cc3cfac42a6d9553bcd8764e
aliciacilmora <akashutosh659@gmail.com>
2025-07-16 17:41:07 +0530

    changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,7 +7,7 @@ const Search = ({ searchTerm, setSearchTerm }) => {
         <img src="search.svg" alt="search"/>
         <input 
         type="text"
-        placeholder='Search through thousands of movies'
+        placeholder='Search through thousands of movies and find out the best'
         value={searchTerm}
         onChange={(event) => setSearchTerm(event.target.value)}
         />
